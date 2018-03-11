63c94418cc83af85f662453b296208cf3a287a16
Shara Soguilon <shara@kalibrr.com>
2018-03-11 21:06:42 +0800

    Identidock-1

---
+++ apps/identidock-1/cmd.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+cd files
+
+if [ "$ENV" == 'DEV' ]; then
+  echo "Running Development Environment"
+  exec python "identidock_1.py"
+elif [ "$ENV" = 'UNIT' ]; then
+  echo "Running Unit Tests"
+  exec python "test_identidock_1.py"
+else
+  echo "Running Production Environment"
+  exec uwsgi --http 0.0.0.0:9090 --wsgi-file identidock_1.py --callable app --stats 0.0.0.0:9191
+fi
