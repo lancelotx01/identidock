8f79900cc074e414f06cb29877c7ed4605730022
Shara Soguilon <shara@kalibrr.com>
2018-03-11 23:32:48 +0800

    full path

--- apps/identidock-1/cmd.sh
+++ apps/identidock-1/cmd.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 set -e
 
-cd /app
-
 if [ "$ENV" == 'DEV' ]; then
   echo "Running Development Environment"
-  exec python "identidock_1.py"
+  exec python "/app/identidock_1.py"
 elif [ "$ENV" = 'UNIT' ]; then
   echo "Running Unit Tests"
-  exec python "test_identidock_1.py"
+  exec python "/app/test_identidock_1.py"
 else
   echo "Running Production Environment"
   exec uwsgi --http 0.0.0.0:9090 --wsgi-file identidock_1.py --callable app --stats 0.0.0.0:9191
